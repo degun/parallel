6c0d1f3590f39eb1e40eb02a30b6be6e98b8d2d0
degun <pretty.lonely.place@hotmail.com>
2020-02-24 18:16:29 +0100

    fixed on firefox. try to fix on mobile.

--- js/funk.js
+++ js/funk.js
@@ -4,8 +4,8 @@ window.addEventListener("load", function(){
     const h = window.screen.height;
     const parallel = document.querySelector(".parallel svg");
     const parallelContainer = document.querySelector(".parallel");
-    parallelContainer.style.height = this.getComputedStyle(parallel).height
-    this.console.log(this.getComputedStyle(parallel).height);
+    parallelContainer.style.height = this.getComputedStyle(parallel).height;
+
     for(let i = 0; i<7;i++){
         parallel.style.zIndex = 7;
         const p = parallel.cloneNode(true);
@@ -13,9 +13,9 @@ window.addEventListener("load", function(){
         parallelContainer.append(p);
     }
     parallels = document.querySelectorAll(".parallel svg");
-    document.addEventListener("wheel", e => {
-        const d = e.deltaY;
-        const direction = e.deltaY > 0 ? "down" : "up";
+
+    function parallelMove(direction, d){
+        if(Math.abs(d) < 5){d < 0 ? d -= 50 : d+=50}
         deg = deg + (d/10);
         
         if(deg < 0){deg =0}
@@ -68,5 +68,62 @@ window.addEventListener("load", function(){
                 })
             }
         }
+    }
+
+    document.addEventListener('touchstart', handleTouchStart, false);        
+    document.addEventListener('touchmove', handleTouchMove, false);
+
+    var xDown = null;                                                        
+    var yDown = null;
+
+    function getTouches(evt) {
+    return evt.touches ||             // browser API
+            evt.originalEvent.touches; // jQuery
+    }                                                     
+
+    function handleTouchStart(evt) {
+        const firstTouch = getTouches(evt)[0];                                      
+        xDown = firstTouch.clientX;                                      
+        yDown = firstTouch.clientY;                                      
+    };   
+
+
+    function handleTouchMove(evt) {
+        if ( ! xDown || ! yDown ) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;                                    
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = xDown - xUp;
+        var yDiff = yDown - yUp;
+
+        const d = yUp;
+
+        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+            if ( xDiff > 0 ) {
+                /* left swipe */ 
+            } else {
+                /* right swipe */
+            }                       
+        } else {
+            if ( yDiff > 0 ) {
+                direction = "up"
+            } else { 
+                direction = "down"
+            }                                                                 
+        }
+        parallelMove(direction, d)
+        /* reset values */
+        xDown = null;
+        yDown = null;                                             
+    };
+
+    
+    document.addEventListener("wheel", e => {
+        const d = e.deltaY;
+        const direction = d > 0 ? "down" : "up";
+        parallelMove(direction, d)
     })
 });
